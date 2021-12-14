47a96891dc50f33e18c141a3d1c6bd2a45a00431
Anton Guzenko <guzik8989@gmail.com>
2021-12-15 01:53:29 +0200

    added view structures

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -18,20 +18,7 @@ struct ContentView: View {
             Color("BackgroundColor")
             .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
-                    Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
-                        .bold()
-                        .kerning(2.0)
-                        .multilineTextAlignment(.center)
-                        .lineSpacing(4.0)
-                        .font(.footnote)
-                        .padding(.leading, 30.0)
-                        .padding(.trailing, 30.0)
-                        .foregroundColor(Color("TextColor"))
-                   Text(String(game.target))
-                        .kerning(-1.0)
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                        .foregroundColor(Color("TextColor"))
+                   InstructionsView(game: $game)
                     HStack {
                         Text("1")
                             .bold()
@@ -72,6 +59,21 @@ struct ContentView: View {
         }
             
     }
+struct InstructionsView: View {
+    
+    @Binding var game: Game
+    
+    
+    
+    var body: some View{
+        VStack {
+        InstructionText(text: "🎯🎯🎯\nPut the Bullseye as close as you can to")
+            .padding(.leading, 30.0)
+            .padding(.trailing, 30.0)
+        BigNumberText(text: String(game.target))
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
