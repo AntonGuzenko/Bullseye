bfa000407b9b8ae87ad812f977d4a1f5488ce2ee
AntonGuzenko <guzik8989@gmail.com>
2021-12-15 16:00:40 +0200

    added structures InstructionViews,HitMe, Slider

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -18,52 +18,18 @@ struct ContentView: View {
             Color("BackgroundColor")
             .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
-                   InstructionsView(game: $game)
-                    HStack {
-                        Text("1")
-                            .bold()
-                            .foregroundColor(Color("TextColor"))
-                        Slider(value: $sliderValue, in: 1.0...100.0)
-                        Text("100")
-                            .bold()
-                            .foregroundColor(Color("TextColor"))
-                        
-                    }
-                    .padding()
-                
-                    Button(action: {
-                        print("hello Anton")
-                        alertIsVisible = true
-                    }) {
-                        Text("Hit me".uppercased())
-                        .bold()
-                        .font(.title3)
-                    }
-                    .padding(20.0)
-                    .background(
-                        ZStack {
-                            Color("ButtonColor")
-                            LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
-                            
-                        })
-                    .foregroundColor(Color.white)
-                    .cornerRadius(21.0)
-                    .alert(isPresented: $alertIsVisible) {
-                        let roundedValue = Int(sliderValue.rounded())
-                        
-                        return Alert(title: Text("Hello, there"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points"), dismissButton: .default(Text("Awesome!")))
-                    }
-            }
-        }
-            
-        }
-            
+              InstructionsView(game: $game)
+              SliderView(sliderValue: $sliderValue)
+              HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
+       }
     }
+            
+  }
+            
+}
 struct InstructionsView: View {
     
     @Binding var game: Game
-    
-    
     
     var body: some View{
         VStack {
@@ -74,6 +40,54 @@ struct InstructionsView: View {
         }
     }
 }
+
+struct SliderView: View {
+    @Binding  var sliderValue: Double
+    
+    var body: some View{
+        HStack {
+            SliderLabelText(text: "1")
+            Slider(value: $sliderValue, in: 1.0...100.0)
+            SliderLabelText(text: "100")
+
+            
+        }
+        .padding()
+    }
+}
+
+struct HitMeButton: View {
+    @Binding  var alertIsVisible: Bool
+    @Binding  var sliderValue: Double
+    @Binding  var game: Game
+    
+    
+    var body: some View{
+        Button(action: {
+            print("hello Anton")
+            alertIsVisible = true
+        }) {
+            Text("Hit me".uppercased())
+            .bold()
+            .font(.title3)
+        }
+        .padding(20.0)
+        .background(
+            ZStack {
+                Color("ButtonColor")
+                LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                
+            })
+        .foregroundColor(Color.white)
+        .cornerRadius(21.0)
+        .alert(isPresented: $alertIsVisible) {
+            let roundedValue = Int(sliderValue.rounded())
+            
+            return Alert(title: Text("Hello, there"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points"), dismissButton: .default(Text("Awesome!")))
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
